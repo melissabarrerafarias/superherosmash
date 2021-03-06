53f319b160b2057aa09713955fce9833138ea082
Adam Barron <adambarron@me.com>
2021-03-06 12:59:10 -0800

    moved schemas folder to right level, modified server.js to utilize Apollo Server, typeDefs, and resolvers. Verified with a 'helloworld' example that server works properly in GraphQL playground

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,7 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql``;
+const typeDefs = gql`
+
+`;
 
 module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -10,5 +19,6 @@ app.use(express.json());
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
