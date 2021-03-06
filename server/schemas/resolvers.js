73c22a5ba0298ef17f479f1db7fdf98a5fc82990
Adam Barron <adambarron@me.com>
2021-03-06 15:34:59 -0800

    created User model, set up logic for users in typeDefs and resolvers and verififed functionality in GraphQL playground

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,27 @@
+const User = require('../models/User');
+
 const resolvers = {
     Query: {
+        // get all users
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+        },
+        // get a user by username
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password')
+        },
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
 
+            return user;
+        },
+        login: async () => {
+
+        }
     }
 };
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,21 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+    }
 
+    type Query {
+        users: [User]
+        user(username: String!): User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): User
+        addUser(username: String!, email: String!, password: String!): User
+    }
 `;
 
 module.exports = typeDefs;
