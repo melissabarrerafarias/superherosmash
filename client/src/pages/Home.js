320c1005c0a120439bdc8773d91aa7934c56380e
AnjinIsmail <anjin.ismail@gmail.com>
2021-03-09 20:29:01 -0800

    changed homepage images

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -14,18 +14,19 @@ const Home = () => {
            <div className ="fade-in">
         <section className="hero-section">
           <div className="cgrid">
-            <a className="card" href="#">              {/* <div className="cardbg" style={{backgroundImage: 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuIFQdxvUqpgude0oKs3oZjHAEObeR-o7ieQ&usqp=CAU'}} /> */}
+            <a className="card" href="#">
+              <div className="cardbg" style={{backgroundImage: 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuIFQdxvUqpgude0oKs3oZjHAEObeR-o7ieQ&usqp=CAU'}} />
 
-              <div className="cardbg" style={{backgroundImage: 'url(https://www.pngarts.com/files/2/Ben-Affleck-Batman-PNG-Image-Background.png'}} />
+              {/* <div className="cardbg" style={{backgroundImage: 'url(https://www.pngarts.com/files/2/Ben-Affleck-Batman-PNG-Image-Background.png'}} /> */}
 
               <div className="content">
                 <h3 className="textcard">Stats</h3>
               </div>
             </a>
             <a className="card" href="/BattleGround">
-              {/* <div className="cardbg" style={{backgroundImage: 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfvjnkkT6fTMAwO4uX_46QeI2d-2N_3sXTpQ&usqp=CAU)'}} /> */}
+              <div className="cardbg" style={{backgroundImage: 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfvjnkkT6fTMAwO4uX_46QeI2d-2N_3sXTpQ&usqp=CAU)'}} />
               {/* <div className="cardbg" style={{backgroundImage: 'url(https://freepngimg.com/thumb/marvel_vision/3-2-marvel-vision-png-file.png'}} /> */}
-              <div className="cardbg" style={{backgroundImage: 'url(https://freepngimg.com/thumb/marvel_vision/3-2-marvel-vision-png-file.png'}} />
+              {/* <div className="cardbg" style={{backgroundImage: 'url(https://freepngimg.com/thumb/marvel_vision/3-2-marvel-vision-png-file.png'}} /> */}
 
 
               <div className="content">
@@ -33,16 +34,16 @@ const Home = () => {
               </div>
             </a>
             <a className="card" href="#">
-              <div className="cardbg" style={{backgroundImage: 'url(https://www.searchpng.com/wp-content/uploads/2019/03/Hulk-PNG-Images-Transparent.png'}} />
-              {/* <div className="cardbg" style={{backgroundImage: 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXEMHwHJIl0fV9US4V3qB05i0v8ERFbn3zew&usqp=CAU)'}} /> */}
+              {/* <div className="cardbg" style={{backgroundImage: 'url(https://www.searchpng.com/wp-content/uploads/2019/03/Hulk-PNG-Images-Transparent.png'}} /> */}
+              <div className="cardbg" style={{backgroundImage: 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXEMHwHJIl0fV9US4V3qB05i0v8ERFbn3zew&usqp=CAU)'}} />
 
               <div className="content">
                 <h3 className="textcard">Discussion!</h3>
               </div>
             </a><a className="card" href="/Login">
-            <div className="cardbg" style={{backgroundImage: 'url(https://i.pinimg.com/originals/f7/2d/8d/f72d8dd0e24aba42ce77d840076a0d06.png'}} />
+            {/* <div className="cardbg" style={{backgroundImage: 'url(https://i.pinimg.com/originals/f7/2d/8d/f72d8dd0e24aba42ce77d840076a0d06.png'}} /> */}
 
-              {/* <div className="cardbg" style={{backgroundImage: 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsmB4_n4_HxQ7EGDm4tcYZfVny2mJBjwVIMA&usqp=CAU)'}} /> */}
+              <div className="cardbg" style={{backgroundImage: 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsmB4_n4_HxQ7EGDm4tcYZfVny2mJBjwVIMA&usqp=CAU)'}} />
               <div className="content">
                 <h3 className="textcard">Login/Signup</h3>
               </div>
