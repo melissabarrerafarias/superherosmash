7ed1d99834587f6b7dbe656cdc5b219d1530bffc
treguv <vladislavtregubov00@gmail.com>
2021-03-11 01:41:19 -0800

    Added style to the cards and display the info

--- client/src/components/BattelgroundMock/Herocard.js
+++ client/src/components/BattelgroundMock/Herocard.js
@@ -2,6 +2,9 @@ import React from "react";
 import { useQuery } from "@apollo/react-hooks";
 import { QUERY_HEROS } from "../../utils/queries";
 
+//semantic ui
+import { Card, Icon, Image } from "semantic-ui-react";
+
 const HeroCard = ({ hero }) => {
   const { loading, data } = useQuery(QUERY_HEROS);
   if (loading) {
@@ -10,23 +13,71 @@ const HeroCard = ({ hero }) => {
     console.log("This heros name is ");
     console.log(hero);
     return (
-      <div className="HeroCard">
-        Hero Name: {hero.name} <br></br>
-        Hero Id:<br></br>
-        Hero strength: {hero.strength}
-        <br></br>
-        Hero durability: {hero.durability}
-        <br></br>
-        Hero power: {hero.power}
-        <br></br>
-        Hero combat: {hero.combat}
-        <br></br>
-        Hero speed: {hero.speed}
-        <br></br>
-        <img
-          style={{ width: "100px", height: "100px" }}
-          src={hero.imgurl}
-        ></img>
+      <div className="wrapper ">
+        {/* <Card className="herocard">
+          <Image id="cardImage" src={hero.name} wrapped ui={false} />
+          <Card.Content id="remove-padding">
+            <Card.Header>{hero.name}</Card.Header>
+            <Card.Meta>
+              <span className="strength">Strength: {hero.strength}</span>
+              <br></br>
+              <span className="strength">Durability: {hero.durability}</span>
+              <br></br>
+              <span className="strength">Speed: {hero.speed}</span>
+              <br></br>
+              <span className="strength">Power: {hero.power}</span>
+              <br></br>
+              <span className="strength">Combat: {hero.combat}</span>
+              <br></br>
+            </Card.Meta>
+            <Card.Description>Biography: {hero.biography}</Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name="user" />
+              VOTES: NULL
+            </a>
+          </Card.Content>
+        </Card> */}
+        <div class="card">
+          <img
+            src={hero.imgurl}
+            alt="Person"
+            className="card__image offsetMove"
+          ></img>
+          <p className="offsetMove card__name">{hero.name}</p>
+          <div class="grid-container offsetMove">
+            <div class="grid-child-posts">Strength: {hero.strength}</div>
+            <div class="grid-child-posts">Durability: {hero.durability}</div>
+            <div class="grid-child-posts">Speed: {hero.speed}</div>
+            <div class="grid-child-posts">Power: {hero.power}</div>
+            <div class="grid-child-posts">Combat: {hero.combat}</div>
+          </div>
+          {/* <ul class="social-icons">
+            <li>
+              <a href="#">
+                <i class="fa fa-instagram"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fa fa-twitter"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fa fa-linkedin"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fa fa-codepen"></i>
+              </a>
+            </li>
+          </ul> */}
+          <button className="offsetMove btn draw-border">Vote</button>
+          <button className="offsetMove btn draw-border">More Info</button>
+        </div>
       </div>
     );
   }
--- client/src/components/BattelgroundMock/index.js
+++ client/src/components/BattelgroundMock/index.js
@@ -3,7 +3,7 @@ import { useSpring, animated as a } from "react-spring";
 import Arena from "./Arena";
 // Hero stuff
 import { useQuery } from "@apollo/react-hooks";
-import { QUERY_HEROS, QUERY_HERO_BY_ID } from "../../utils/queries";
+import { QUERY_HERO_BY_ID } from "../../utils/queries";
 
 const HEROS = [
   {
@@ -14,6 +14,7 @@ const HEROS = [
     power: "",
     combat: "",
     durability: "",
+    biography: "",
     imgurl: "",
   },
   {
@@ -24,27 +25,16 @@ const HEROS = [
     power: "",
     combat: "",
     durability: "",
+    biography: "",
     imgurl: "",
-  },
-  {
-    id: 3,
-    name: "Hero3",
-  },
-  {
-    id: 4,
-    name: "Hero4",
-  },
-  {
-    id: 5,
-    name: "Hero5",
   },
 ];
 
 const BattleGround = () => {
   //let heroNumOne = Math.floor(Math.random() * (730 - 1) + 1);
   // let heroNumTwo = Math.floor(Math.random() * (730 - 1) + 1);
-  let num1 = 10; // id of the first hero to display
-  let num2 = 20; // id of the second hero to display
+  let num1 = 510; // id of the first hero to display
+  let num2 = 478; // id of the second hero to display
   const props = useSpring({ opacity: 1, from: { opacity: 0 } });
   const { loading, data } = useQuery(QUERY_HERO_BY_ID, {
     variables: { id: num1 },
@@ -76,13 +66,19 @@ const BattleGround = () => {
 
 function populateHeroObject(heroNum, heroData) {
   let currentHero = HEROS[heroNum - 1];
+  console.log(heroData);
   currentHero.id = heroData.getHeroById.id;
-  currentHero.name = heroData.getHeroById.name;
+  currentHero.name = trimWhiteSpace(heroData.getHeroById.name);
   currentHero.speed = heroData.getHeroById.speed;
   currentHero.power = heroData.getHeroById.power;
   currentHero.combat = heroData.getHeroById.combat;
   currentHero.durability = heroData.getHeroById.durability;
   currentHero.strength = heroData.getHeroById.strength;
   currentHero.imgurl = heroData.getHeroById.imgurl;
+  currentHero.biography = heroData.getHeroById.biography;
+  console.log(heroData.getHeroById.biography); //TODO Data is here on back end but not front for some reason
+}
+function trimWhiteSpace(stringToTrim) {
+  return stringToTrim.trim();
 }
 export default BattleGround;
