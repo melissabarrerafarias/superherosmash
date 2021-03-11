e1d2dc250dfaed63e599a6467176ab50a6fb2179
AnjinIsmail <anjin.ismail@gmail.com>
2021-03-10 23:48:46 -0800

    tested the delete/revert back none working code

--- client/src/pages/MyThreads.js
+++ client/src/pages/MyThreads.js
@@ -64,4 +64,70 @@ const MyThreads = () => {
     );
 };
 
+export default MyThreads;import React, { useState } from 'react';  
+import { Link } from 'react-router-dom';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import { QUERY_ME } from '../utils/queries';
+import { DELETE_COMMENT } from '../utils/mutations';
+
+
+const MyThreads = () => {
+    const { loading, data } = useQuery(QUERY_ME);
+    const [deleteComment, { error }] = useMutation(DELETE_COMMENT);
+
+    const user = data?.me || {};
+
+    const handleDelete = async event => {
+        event.preventDefault();
+
+        let commentId = user.data.comment._id
+        console.log(commentId)
+
+        // try {
+        //     await deleteComment({ variables: { commentId } });
+        // }
+        // catch (e) {
+        //     console.log(e);
+        // }
+    };
+
+    if (loading) {
+        return <div>Please hold on as we reheat our coffee...</div>;
+    }
+
+    return (
+        <div>
+            <div>
+                <h2>
+                    {user.username}'s threads
+          </h2>
+            </div>
+
+            <div>
+                <h3>Look back at what you said!</h3>
+                {user.comments &&
+                    user.comments.map(comment => (
+                        <div key={comment._id}>
+                            <p>
+                                {comment.username} commented on {comment.createdAt}
+                            </p>
+                            <Link to={`/comment/${comment._id}`}>
+                                <div>
+                                    <p>{comment.commentBody}</p>
+                                    <p>
+                                        Replies in this thread: {comment.replyCount} - Go to{' '}
+                                        {comment.replyCount ? 'see' : 'start'} the discussion!
+                            </p>
+                                </div>
+                            </Link>
+                            <div>
+                                <button onClick={handleDelete}>Delete</button>
+                            </div>
+                        </div>
+                    ))}
+            </div>
+        </div>
+    );
+};
+
 export default MyThreads;
