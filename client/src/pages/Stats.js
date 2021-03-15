e7615193f4ad72f189cf0f4423fbd111bffff279
AnjinIsmail <anjin.ismail@gmail.com>
2021-03-15 16:25:55 -0700

    styled battleG and Stats(background)

--- client/src/pages/Stats.js
+++ client/src/pages/Stats.js
@@ -1,9 +1,24 @@
 import React from "react";
 // import { useQuery } from "@apollo/react-hooks";
 // import { QUERY_HEROS } from "../utils/queries";
+import backgroundImage from "../../src/img/hulk.png";
+
 const StatsPage = () => {
   // const [loading, data] = useQuery(QUERY_HEROS);
-  return <div>"Hello World"</div>;
+  return (
+  <body
+        style={{
+          backgroundSize: "cover",
+          backgroundImage: `url(${backgroundImage})`,
+          minHeight: "1000px" // added minheight for image full cover of page (can be taken off)
+        }}
+      >
+<div>
+  'hello world'
+</div>
+</body>
+       
+       )
 };
 
 export default StatsPage;
