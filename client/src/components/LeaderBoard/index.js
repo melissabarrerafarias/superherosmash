bad17848b1d632b9c75b2e9db5c48b0a38ccf0ea
Karl-Johan Westhoff <karl.johan.westhoff@gmail.com>
2021-03-16 10:53:06 -0700

    Leaderboard implemented

---
+++ client/src/components/LeaderBoard/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { QUERY_HEROS} from "../../utils/queries";
+import "./style.css";
+
+const LeaderBoard = () => {
+  const { loading, data } = useQuery(QUERY_HEROS);
+
+  if (loading) {
+    console.log("Loading ...");
+    return <div>L O A D I N G</div>;
+  } else {
+    const heros = data.getAllHeros.sort((a, b) => (a.votes > b.votes ? -1 : 1));
+
+
+
+    return (
+      <div className="leader_board_container">
+        {heros.map((hero) => {
+          return (
+            <div key={hero.id} className="scorecard">
+
+              <p id="heroName" className="scorecard__name">
+                {hero.name}
+              </p>
+              <div className = "votes_bar">
+                <div className="container-bar">
+                  <div
+                    className="skill "
+                    style={{ width: `${hero.votes}%` }}
+                  ></div>
+                </div>
+                <p className = "scorecard__score">
+                {hero.votes} votes
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+};
+
+export default LeaderBoard;
--- client/src/pages/Stats.js
+++ client/src/pages/Stats.js
@@ -1,37 +1,23 @@
 import React from "react";
-import { useQuery } from "@apollo/react-hooks";
-import { QUERY_HEROS } from "../utils/queries";
 import backgroundImage from "../../src/img/hulk.png";
+import LeaderBoard from "../components/LeaderBoard"
 
 const StatsPage = () => {
-  const {loading, data} = useQuery(QUERY_HEROS);
   
-
-  if(loading){
-  console.log("Loading ...");
-  return(<div>L O A D I N G</div>)
-  } else {
-
-  console.log(data)
-  for(const dat of data{
-    console.log(dat)
-  }
-  
-  
-  return (
-  <body
+    return (
+      <body
         style={{
           backgroundSize: "cover",
           backgroundImage: `url(${backgroundImage})`,
-          minHeight: "1000px" // added minheight for image full cover of page (can be taken off)
+          minHeight: "1000px", // added minheight for image full cover of page (can be taken off)
         }}
       >
-<div>
-  'hello world'
-</div>
-</body>
-       
-       )
+
+      <LeaderBoard></LeaderBoard>
+
+      </body>
+    );
+  
 };
 
 export default StatsPage;
