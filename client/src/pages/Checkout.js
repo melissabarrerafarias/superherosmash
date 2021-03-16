c4a2eba855fc529fe7cc413e2fc43c2a24669379
Adam Barron <adambarron@me.com>
2021-03-15 21:14:08 -0700

    moved Stripe wrapper from App.js to Checkout.js to try to avoid styling conflicts

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -1,9 +1,16 @@
 import React from "react";
 import Donation from "../components/Donation";
 
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_API_KEY);
+
 const Checkout = () => {
   return (
-    <Donation></Donation>
+    <Elements stripe={stripePromise}>
+      <Donation></Donation>
+    </Elements>
   ) 
 };
 
