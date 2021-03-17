3c7522cb81d2d9cb250a71cdc140203a2e05cc09
unknown <melissikiss@gmail.com>
2021-03-16 22:30:26 -0700

    basic footer created

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Checkout from "./pages/Checkout";
 import Home from "./pages/Home";
 import StatsPage from "./pages/Stats";
 import NotFound from './pages/NotFound';
+import Footer from './components/Footer'; 
 //Semantic ui
 import "semantic-ui-css/semantic.min.css";
 
@@ -52,6 +53,7 @@ function App() {
               <Route component={NotFound} />
             </Switch>
           </div>
+          <Footer/>
         </div>
 
       </Router>
---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../Footer/footer.css';
+
+
+const Footer = () => {
+    return (
+        <div class="footer">
+            <div className="creators">
+                <a href='https://github.com/KJWesthoff' className="creator">Karl-Johan Westhoff</a>
+                <a href='https://github.com/treguv' className="creator">Vladislav Tregubov</a>
+                <a href='https://github.com/AnjinIsmail' className="creator">Anji Ismail</a>
+                <a href='https://github.com/barron-a' className="creator">Adam Barron</a>
+                <a href="https://github.com/melissabarrerafarias" className="creator">Melissa Barrera</a>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
