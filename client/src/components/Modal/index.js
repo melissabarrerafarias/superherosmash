683f507115e7b99a5e7e2fabcd68410a159f8ffa
unknown <melissikiss@gmail.com>
2021-03-17 12:03:47 -0700

    media queries for modal created

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -7,7 +7,8 @@ function Modal({ handler }) {
         <div className="modal" >
             <button className="close" type="button" onClick={() => handler(false)}><i class="far fa-window-close"></i></button>
             <div className="modal-creators">
-            <h1 className="meet-us">Meet the creators:</h1>
+            <h1 className="meet-us">Meet the creators<p><i class="fab fa-github"></i></p></h1>
+           
                 <a href='https://github.com/KJWesthoff' className="modal-creator">Karl-Johan Westhoff</a>
                 <a href='https://github.com/treguv' className="modal-creator">Vladislav Tregubov</a>
                 <a href='https://github.com/AnjinIsmail' className="modal-creator">Anji Ismail</a>
