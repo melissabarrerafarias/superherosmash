f36975f6feab920a3314a71600a51f5c6808df69
Karl-Johan Westhoff <karl.johan.westhoff@gmail.com>
2021-03-18 15:48:00 -0700

    modal votes working

--- client/src/components/LeaderBoard/index.js
+++ client/src/components/LeaderBoard/index.js
@@ -1,9 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import Modal from "react-modal";
 import { useQuery } from "@apollo/react-hooks";
-import { QUERY_HEROS} from "../../utils/queries";
+import { QUERY_HEROS } from "../../utils/queries";
+
 import "./style.css";
 
+// Dig the votes out of the hero..
+const parseVotes = (voteObjs) => {
+  const userNames = voteObjs.map((obj) => obj.username);
+
+  const map = userNames.reduce(
+    (acc, e) => acc.set(e, (acc.get(e) || 0) + 1),
+    new Map()
+  );
+
+  //console.info([...map.keys()])
+  //console.info([...map.values()])
+  //console.info([...map.entries()])
+
+  const res = [...map.entries()];
+
+  // let str = "";
+  // for (let p of res) {
+  //   str += `${p[0]} has given ${p[1]} votes `;
+  // }
+  console.log(res);
+  return res;
+};
+
+// Modal Config
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+// Main functional component
 const LeaderBoard = () => {
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  // Hooks for modal data
+  const [currentHero, setCurrentHero] = useState({});
+
+  const openModal = (H) => {
+    setCurrentHero(H);
+
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+  }
+
   const { loading, data } = useQuery(QUERY_HEROS);
 
   if (loading) {
@@ -11,71 +70,58 @@ const LeaderBoard = () => {
     return <div>L O A D I N G</div>;
   } else {
     const heros = data.getAllHeros.sort((a, b) => (a.votes > b.votes ? -1 : 1));
-   
-
-    const parseVotes = (voteObjs) => {
-      const userNames = voteObjs.map(obj => obj.username)
-
-      const map = userNames.reduce((acc, e) => acc.set(e, (acc.get(e) || 0) + 1), new Map());
-      
-      //console.info([...map.keys()])
-      //console.info([...map.values()])
-      //console.info([...map.entries()])
-
-      const res =  [...map.entries()]
-      
-      let str = ""
-      for(let p of res){
-        str += `${p[0]} has given ${p[1]} votes `
-      }
-
-      
-
-      return str
-
-
-    }
-
-  
-    
-
-
-    
-
-    
-
-
+    console.log(heros);
     return (
-        <div>
-        <div className="leader-title">Leader Board </div>
-      <div className="ui centered card" id="leaderboard-card">
-        <div className="content" id="leaderboard-card-content">
-
-      
-        {heros.map((hero) => {
-          return (
-            <div key={hero.id} className="scorecard">
-
-              <p id="heroName" className="scorecard__name">
-                {hero.name}
-              </p>
-              <div className = "votes_bar">
-                <div className="container-bar">
-                  <div
-                    className="skill "
-                    style={{ width: `${hero.votes}%` }}
-                  ></div>
-                </div>
-                <p className = "scorecard__score">
-                {hero.votes} votes, {parseVotes(hero.voteObjs)}
+      <div id="leaderBoard">
+        <Modal
+          isOpen={modalIsOpen}
+          onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+          <h2>{currentHero.name}</h2>
+          <button onClick={closeModal}>close</button>
+          <div>
+            <p>Total: {currentHero.votes} votes</p>
+            <p className="scorecard__score">
+              {parseVotes(currentHero.voteObjs).map((vote) => (
+                <p>
+                  {vote[0]} voted {vote[1]} times
                 </p>
-              </div>
-            </div>
-          );
-        })}
+              ))}
+            </p>
+          </div>
+        </Modal>
+
+        <div className="leader-title">Leader Board </div>
+        <div className="ui centered card" id="leaderboard-card">
+          <div className="content" id="leaderboard-card-content">
+            {heros.map((hero) => {
+              return (
+                <div
+                  key={hero.id}
+                  onClick={() => openModal(hero)}
+                  className="scorecard"
+                >
+                  <p id="heroName" className="scorecard__name">
+                    {hero.name}
+                  </p>
+                  <div className="votes_bar">
+                    <div className="container-bar">
+                      <div
+                        className="skill "
+                        style={{ width: `${hero.votes}%` }}
+                      ></div>
+                    </div>
+                    <p className="scorecard__score">{hero.votes} votes</p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
-      </div>
-       </div>
     );
   }
 };
